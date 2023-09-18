01173af3d711c95b9575afd4386d2aedd04fea78
Stephany dos Santos Silva <125597304+StephanySantos15@users.noreply.github.com>
2023-09-18 14:56:33 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,19 @@
+
+function imprime(){
+    let lg = document.getElementById("login").value 
+    let ps = document.getElementById("pass").value
+
+    
+    let senha ="snoopy"
+
+if( ps == senha){
+    alert("Você está logado!")
+}else{
+    alert("Tente novamente")
+}
+
+
+
+
+
+}
